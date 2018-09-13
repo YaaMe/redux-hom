0de10ab9a437655860df0d0463399720d70e86d9
YaaMe <969519545@qq.com>
2018-09-13 19:25:03 +0900

    restore(compose): restore compose from redux

--- src/compose.js
+++ src/compose.js
@@ -1,7 +1,11 @@
 export default function compose(...funcs) {
-  switch (funcs.length) {
-    case 0: return arg => arg;
-    case 1: return funcs[0];
-    default: return funcs.reduce((a, b) => (...args) => a(b(...args)));
-  }
+    if (funcs.length === 0) {
+        return arg => arg;
+    }
+
+    if (funcs.length === 1) {
+        return funcs[0];
+    }
+
+    return funcs.reduce((a, b) => (...args) => a(b(...args)));
 }
